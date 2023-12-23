30f7f6d8cd1fd580a9ab8ff0bb08b88ef782602e
x9y0 <89051449+x9y0@users.noreply.github.com>
2023-12-23 15:47:59 +0100

    Update clock.js

--- clock/clock.js
+++ clock/clock.js
@@ -1,8 +1,19 @@
-// Function to update the time dynamically (optional)
-function updateTime() {
-    const time = new Date().toLocaleTimeString('en-US', { hour12: false });
-    document.querySelector('h1').textContent = time;
+function updateClock() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  // Add leading zeros to the minutes and seconds
+  minutes = (minutes < 10 ? "0" : "") + minutes;
+  seconds = (seconds < 10 ? "0" : "") + seconds;
+
+  var time = hours + ":" + minutes + ":" + seconds;
+  document.getElementById("clock").innerText = time;
 }
 
-// Call the updateTime function every second
-setInterval(updateTime, 1000);
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Initial call to display the clock immediately
+updateClock();
